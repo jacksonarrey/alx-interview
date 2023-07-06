cb010a1e2eb689ee53e4aa0b9599c32f99627df2
jacksonarrey <agborarrey93@gmail.com>
2023-07-06 23:56:08 +0000

    0x03. ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getlistStudents() {
+  return [{id: "1", firstName: "Guillaume", location: "San Francisco"}
+	  {id: "2", firstName: "James", location: "Columbia"}
+	  {id: "5", firstName: "Serena", location: "San Francisco"}];
+}
+
+const students = getlistStudents()
+console.log(students);
